ae70f46b77d039682edfc2d4d8a1acc2e9fc1631
Tolunay Toprakkaya <tolunaytoprakkaya@gmail.com>
2022-02-21 19:33:23 +0300

    TransactionController base url changed

--- src/main/java/com/sha/springbootmicroservicetransaction/controller/TransactionController.java
+++ src/main/java/com/sha/springbootmicroservicetransaction/controller/TransactionController.java
@@ -7,7 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("api/transaction")
+@RequestMapping("/api/transaction")
 public class TransactionController {
 
     private final ITransactionService transactionService;
